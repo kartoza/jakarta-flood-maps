807205185a41f5a005bb874a6e32ec1335cfda7d
Tim Sutton <tim@linfiniti.com>
2015-01-21 17:25:06 +0200

    In progress making pdf printing work

--- crons/pdf_report_generator.py
+++ crons/pdf_report_generator.py
@@ -3,51 +3,27 @@
 import os
 import sys
 from qgis.core import (
-    QgsRectangle,
     QgsApplication,
-    QgsMapRenderer,
-    QgsPoint,
-    QgsField,
-    QgsFeature,
-    QgsGeometry,
-    QgsVectorLayer,
-    QgsRaster,
-    QgsRasterLayer,
-    QgsDataSourceURI,
-    QgsVectorFileWriter,
-    QgsCoordinateReferenceSystem,
     QgsProject,
     QgsComposition,
     QgsMapLayerRegistry,
     QgsPalLabeling,
-    QgsProviderRegistry,
-    QgsFeatureRequest,
-    QgsVectorDataProvider,
-    QgsMapSettings
+    QgsMapSettings,
 )
+from qgis.gui import QgsMapCanvas, QgsLayerTreeMapCanvasBridge
 from PyQt4.QtCore import (
     QCoreApplication,
-    QSizeF,
-    QObject,
-    QVariant,
     QFileInfo,
-    QUrl,
     QSize,
-    Qt,
-    QTranslator
 )
-from PyQt4.QtGui import QPrinter, QPainter
-
+from PyQt4.QtGui import QPrinter, QPainter, QWidget, QVBoxLayout
 from PyQt4.QtXml import QDomDocument
-
 
 QCoreApplication.setOrganizationName('JakartaFloodMaps')
 QCoreApplication.setOrganizationDomain('kartoza.com')
 QCoreApplication.setApplicationName('JakartaFloodMaps')
 
-#noinspection PyPep8Naming
 gui_flag = True
-# app = QApplication([], gui_flag)
 app = QgsApplication(sys.argv, gui_flag)
 
 # Make sure QGIS_PREFIX_PATH is set in your env if needed!
@@ -66,10 +42,6 @@ pdf_file = os.path.abspath(os.path.join(
     '2015-01-13-12.pdf'
 ))
 
-# Make sure the map layers have all been removed before we
-# start otherwise in batch mode we will get overdraws.
-# noinspection PyArgumentList
-
 project_path = os.path.abspath(os.path.join(
     os.path.dirname(__file__),
     os.path.pardir,
@@ -78,11 +50,17 @@ project_path = os.path.abspath(os.path.join(
     'jakarta_flood_maps.qgs'
 ))
 
-# Load our project
-QgsProject.instance().setFileName(project_path)
-# noinspection PyArgumentList
-# QgsProject.instance().read()
+widget = QWidget()
+canvas = QgsMapCanvas(widget)
+canvas.resize(QSize(400, 400))
 
+print 'Canvas extent before loading project: %s' % canvas.extent().toString()
+# Load our project
+bridge = QgsLayerTreeMapCanvasBridge(
+    QgsProject.instance().layerTreeRoot(), canvas)
+QgsProject.instance().read(QFileInfo(project_path))
+canvas.zoomToFullExtent()
+print 'Canvas extent after loading project: %s' % canvas.extent().toString()
 # Load our template
 template_path = os.path.abspath(os.path.join(
     os.path.dirname(__file__),
@@ -98,19 +76,12 @@ template_file.close()
 document = QDomDocument()
 document.setContent(template_content)
 
-label_engine = QgsPalLabeling()
-renderer = QgsMapRenderer()
-renderer.setLabelingEngine(label_engine)
-
-crs = QgsCoordinateReferenceSystem('EPSG:4326')
-extent = QgsRectangle(1, 1, 10, 10)
-map_settings = QgsMapSettings()
-map_settings.setDestinationCrs(crs)
-map_settings.setCrsTransformEnabled(True)
-map_settings.setExtent(extent)
-map_settings.setOutputSize(QSize(1000, 1000))
+#
+#label_engine = QgsPalLabeling()
+#renderer = QgsMapRenderer()
+#renderer.setLabelingEngine(label_engine)
 # Now set up the composition
-composition = QgsComposition(map_settings)
+composition = QgsComposition(canvas.mapSettings())
 # You can use this to replace any string like this [key]
 # in the template with a new value. e.g. to replace
 # [date] pass a map like this {'date': '1 Jan 2012'}
@@ -119,31 +90,9 @@ substitution_map = {
     'DATE_TIME_END': 'bar'}
 composition.loadFromTemplate(document, substitution_map)
 
-printer = QPrinter()
-printer.setOutputFormat(QPrinter.PdfFormat)
-printer.setOutputFileName(pdf_file)
-printer.setPaperSize(
-    QSizeF(composition.paperWidth(), composition.paperHeight()),
-    QPrinter.Millimeter)
-printer.setFullPage(True)
-printer.setColorMode(QPrinter.Color)
-printer.setResolution(composition.printResolution())
-
-pdfPainter = QPainter(printer)
-paperRectMM = printer.pageRect(QPrinter.Millimeter)
-paperRectPixel = printer.pageRect(QPrinter.DevicePixel)
-composition.render(pdfPainter, paperRectPixel, paperRectMM)
-pdfPainter.end()
-
-
-
-
 # Get the main map canvas on the composition and set
 # its extents to the event.
 map_canvas = composition.getComposerItemById('main-map')
-# map_canvas.setNewExtent(extent)
-map_canvas.renderModeUpdateCachedImage()
-
 # Save a pdf.
 composition.exportAsPDF(pdf_file)
 
