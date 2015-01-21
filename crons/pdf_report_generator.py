41c9181f2b8847e1306790d63fa9bac665c07a6a
Tim Sutton <tim@linfiniti.com>
2015-01-21 12:54:29 +0200

    In progress setting up of pdf renderer

--- crons/pdf_report_generator.py
+++ crons/pdf_report_generator.py
@@ -1,11 +1,11 @@
-__author__ = 'christian'
 # coding=utf-8
-"""Views for layers"""
-import glob
+
 import os
-
-
+import sys
 from qgis.core import (
+    QgsRectangle,
+    QgsApplication,
+    QgsMapRenderer,
     QgsPoint,
     QgsField,
     QgsFeature,
@@ -27,6 +27,7 @@ from qgis.core import (
 )
 from PyQt4.QtCore import (
     QCoreApplication,
+    QSizeF,
     QObject,
     QVariant,
     QFileInfo,
@@ -35,97 +36,115 @@ from PyQt4.QtCore import (
     Qt,
     QTranslator
 )
+from PyQt4.QtGui import QPrinter, QPainter
+
 from PyQt4.QtXml import QDomDocument
 
 
-def render_map():
-    """This is the 'do it all' method to render a pdf.
+QCoreApplication.setOrganizationName('JakartaFloodMaps')
+QCoreApplication.setOrganizationDomain('kartoza.com')
+QCoreApplication.setApplicationName('JakartaFloodMaps')
 
-    :param force_flag: (Optional). Whether to force the
-            regeneration of map product. Defaults to False.
-    :type force_flag: bool
+#noinspection PyPep8Naming
+gui_flag = True
+# app = QApplication([], gui_flag)
+app = QgsApplication(sys.argv, gui_flag)
 
-    :raise Propagates any exceptions.
-    """
-    reports_dir = os.path.abspath(os.path.join(
-        os.path.dirname(__file__),
-        os.path.pardir,
-        'reports',
-    ))
-    shp_file = os.path.join(
-        reports_dir,
-        'shp',
-        '6h',
-        '2015-01-13-12.shp'
-    )
-    pdf_file = os.path.join(
-        reports_dir,
-        'pdf',
-        '6h',
-        '2015-01-13-12.pdf'
-    )
+# Make sure QGIS_PREFIX_PATH is set in your env if needed!
+app.initQgis()
 
-    # Make sure the map layers have all been removed before we
-    # start otherwise in batch mode we will get overdraws.
-    # noinspection PyArgumentList
-    QgsMapLayerRegistry.instance().removeAllMapLayers()
+reports_dir = os.path.abspath(os.path.join(
+    os.path.dirname(__file__),
+    os.path.pardir,
+    'reports',
+))
 
-    layer = QgsVectorLayer(
-        shp_file,
-        'mmi-cities', "ogr")
-    if not layer.isValid():
-        raise ImportError
+pdf_file = os.path.abspath(os.path.join(
+    reports_dir,
+    'pdf',
+    '6h',
+    '2015-01-13-12.pdf'
+))
 
-    project_path = os.path.join(
-        os.path.dirname(__file__),
-        os.path.pardir,
-        'maps',
-        'projects',
-        'jakarta_flood_maps.qgs')
+# Make sure the map layers have all been removed before we
+# start otherwise in batch mode we will get overdraws.
+# noinspection PyArgumentList
 
-    # Load our project
-    QgsProject.instance().setFileName(project_path)
-    # noinspection PyArgumentList
-    QgsProject.instance().read()
+project_path = os.path.abspath(os.path.join(
+    os.path.dirname(__file__),
+    os.path.pardir,
+    'maps',
+    'projects',
+    'jakarta_flood_maps.qgs'
+))
 
-    # noinspection PyArgumentList
-    QgsMapLayerRegistry.instance().addMapLayers([layer])
+# Load our project
+QgsProject.instance().setFileName(project_path)
+# noinspection PyArgumentList
+# QgsProject.instance().read()
 
-    # Load our template
-    template_path = os.path.join(
-        os.path.dirname(__file__),
-        os.path.pardir,
-        'maps',
-        'templates',
-        'jakart_flooded_rw.qpt')
+# Load our template
+template_path = os.path.abspath(os.path.join(
+    os.path.dirname(__file__),
+    os.path.pardir,
+    'maps',
+    'templates',
+    'jakarta_flooded_rw.qpt'
+))
 
-    template_file = file(template_path)
-    template_content = template_file.read()
-    template_file.close()
-    document = QDomDocument()
-    document.setContent(template_content)
+template_file = file(template_path)
+template_content = template_file.read()
+template_file.close()
+document = QDomDocument()
+document.setContent(template_content)
 
-    # Now set up the composition
-    composition = QgsComposition(QgsMapSettings())
+label_engine = QgsPalLabeling()
+renderer = QgsMapRenderer()
+renderer.setLabelingEngine(label_engine)
 
-    # You can use this to replace any string like this [key]
-    # in the template with a new value. e.g. to replace
-    # [date] pass a map like this {'date': '1 Jan 2012'}
-    substitution_map = {
-        'this': 'that',
-        'here': 'there'}
+crs = QgsCoordinateReferenceSystem('EPSG:4326')
+extent = QgsRectangle(1, 1, 10, 10)
+map_settings = QgsMapSettings()
+map_settings.setDestinationCrs(crs)
+map_settings.setCrsTransformEnabled(True)
+map_settings.setExtent(extent)
+map_settings.setOutputSize(QSize(1000, 1000))
+# Now set up the composition
+composition = QgsComposition(map_settings)
+# You can use this to replace any string like this [key]
+# in the template with a new value. e.g. to replace
+# [date] pass a map like this {'date': '1 Jan 2012'}
+substitution_map = {
+    'DATE_TIME_START': 'foo',
+    'DATE_TIME_END': 'bar'}
+composition.loadFromTemplate(document, substitution_map)
 
-    composition.loadFromTemplate(document, substitution_map)
+printer = QPrinter()
+printer.setOutputFormat(QPrinter.PdfFormat)
+printer.setOutputFileName(pdf_file)
+printer.setPaperSize(
+    QSizeF(composition.paperWidth(), composition.paperHeight()),
+    QPrinter.Millimeter)
+printer.setFullPage(True)
+printer.setColorMode(QPrinter.Color)
+printer.setResolution(composition.printResolution())
 
-    # Get the main map canvas on the composition and set
-    # its extents to the event.
-    map_canvas = composition.getComposerItemById('main-map')
-    # map_canvas.setNewExtent(extent)
-    map_canvas.renderModeUpdateCachedImage()
-
-    # Save a pdf.
-    composition.exportAsPDF(pdf_file)
+pdfPainter = QPainter(printer)
+paperRectMM = printer.pageRect(QPrinter.Millimeter)
+paperRectPixel = printer.pageRect(QPrinter.DevicePixel)
+composition.render(pdfPainter, paperRectPixel, paperRectMM)
+pdfPainter.end()
 
 
-if __name__ == "__main__":
-    render_map()
+
+
+# Get the main map canvas on the composition and set
+# its extents to the event.
+map_canvas = composition.getComposerItemById('main-map')
+# map_canvas.setNewExtent(extent)
+map_canvas.renderModeUpdateCachedImage()
+
+# Save a pdf.
+composition.exportAsPDF(pdf_file)
+
+
