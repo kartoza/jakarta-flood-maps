4cb52402df52a32761acd834f02266e1a3b579cb
Tim Sutton <tim@linfiniti.com>
2015-01-25 12:00:25 +0200

    Hacked working version of pdf generator (in progress)

--- deployment/cron-scripts/pdf_report_generator.py
+++ deployment/cron-scripts/pdf_report_generator.py
@@ -3,6 +3,7 @@
 # A simple demonstration of how to print
 # This code is public domain, use if for any purpose you see fit.
 # Tim Sutton 2015
+import os
 
 import sys
 
@@ -12,10 +13,11 @@ from qgis.core import (
     QgsComposition,
     QgsApplication,
     QgsProviderRegistry,
+    QgsCoordinateReferenceSystem,
     QgsVectorLayer,
     QgsRasterLayer)
 
-from qgis.gui import QgsMapCanvas, QgsLayerTreeMapCanvasBridge
+from qgis.gui import QgsMapCanvas, QgsLayerTreeMapCanvasBridge, QgsMapCanvasLayer
 from PyQt4.QtCore import QFileInfo
 from PyQt4.QtXml import QDomDocument
 
@@ -42,6 +44,7 @@ class PdfMaker(object):
             print QgsProviderRegistry.instance().pluginList()
 
         self.canvas = QgsMapCanvas()
+        self.canvas.enableAntiAliasing(True)
 
         self.project_path = project_path
         self.template_path = template_path
@@ -49,12 +52,6 @@ class PdfMaker(object):
     def __del__(self):
         """Destructor."""
         del self.app
-
-    def _load_project(self):
-        """Load the QGIS project."""
-        QgsLayerTreeMapCanvasBridge(
-            QgsProject.instance().layerTreeRoot(), self.canvas)
-        QgsProject.instance().read(QFileInfo(self.project_path))
 
     def _load_template(self):
         """Load the template.
@@ -75,50 +72,30 @@ class PdfMaker(object):
             'DATE_TIME_START': 'foo',
             'DATE_TIME_END': 'bar'}
         composition.loadFromTemplate(document, substitution_map)
+        return composition
 
-    def load_layers(self):
-        """Manually load all the layers listed in the given project.
-
-        This is an alternative to load_project which does not reliably
-        add all layers into the project.
-
-        :return: A list of QgsMapLayer instances.
-        :rtype: list
+    def _iterate_layer_elements(self):
+        """Iterator for all layers in a QGIS project file.
         """
-        layers = []
-        for layer in self._iterate_layers('maplayer'):
-            layer_type = self._getAttr(layer, 'type').value()
-            if layer_type == 'vector':
-                qgsLayer = QgsVectorLayer()
-            elif layer_type == 'raster':
-                qgsLayer = QgsRasterLayer()
-
-            # read layer from XML
-            element = layer.toElement()
-            if not(qgsLayer.readLayerXML(element)):
-                raise RuntimeError(
-                    'Layer is not readable: {}'.format(
-                        layer.firstChildElement('id').text()
-                    )
-                )
-            layers.append(qgsLayer)
-        return layers
-
-    def _iterate_layers(self, tag):
-        """Iterator for all layers ina QGIS project file.
-
-        :param tag: Tag to search for.
-        :type tag: str
-        """
+        tag = 'maplayer'
+        print 'Searching for tag: %s' % tag
         project_file = file(self.project_path)
         project_content = project_file.read()
         project_file.close()
         document = QDomDocument()
-        document.setContent(project_content)
+        result = document.setContent(project_content)
+        if not result:
+            raise Exception('DOM loading failed')
+
         elements = document.elementsByTagName(tag)
+        print 'Found %i layers' % elements.size()
+
         for i in xrange(elements.size()):
-            print elements.at(1)
-            yield elements.at(i)
+            print 'Element: %s' % elements.item(i).toDocument().toString()
+            layer = elements.item(i)
+            print 'ID: %s' % layer.firstChildElement('id').text()
+            print 'DataSource: %s' % layer.firstChildElement('datasource').text()
+            yield layer
 
     def _getAttr(self, obj, attr):
         """Get an attribute from a document."""
@@ -128,6 +105,40 @@ class PdfMaker(object):
         attrs = obj.attributes()
         return attrs.namedItem(attr).toAttr()
 
+
+    def _load_layers(self):
+        """Manually load all the layers listed in the given project.
+
+        This is an alternative to load_project which does not reliably
+        add all layers into the project.
+
+        :return: A list of QgsMapLayer instances.
+        :rtype: list
+        """
+        layers = []
+        project_dir = os.path.abspath(
+                os.path.dirname(self.project_path))
+        for item in self._iterate_layer_elements():
+            layer_type = self._getAttr(item, 'type').value()
+            source = item.firstChildElement('datasource').text()
+            source = os.path.abspath(
+                os.path.join(
+                    project_dir,
+                    source
+                )
+            )
+            provider = item.firstChildElement('provider').text()
+            title = item.firstChildElement('title').text()
+            if layer_type == 'vector':
+                layer = QgsVectorLayer(source, title, provider)
+
+            if not layer.isValid():
+                raise Exception('Loaded layer is not valid: %s' % source)
+            QgsMapLayerRegistry.instance().addMapLayer(layer, False)
+            canvas_layer = QgsMapCanvasLayer(layer)
+            layers.append(canvas_layer)
+        return layers
+
     def make_pdf(self, pdf_path):
         """Generate a pdf for the given project and template files.
 
@@ -135,18 +146,19 @@ class PdfMaker(object):
         :type pdf_path: str
 
         """
-        self._load_project()
-        layers = self.load_layers()
-        for item in layers:
-            print item.source()
 
-        for item in QgsMapLayerRegistry.instance().mapLayers():
-            print item.source()
+        layers = self._load_layers()
+        self.canvas.setLayerSet(layers)
 
         if self.canvas.layerCount() < 1:
             print 'No layers loaded from this project, exiting.'
             return
-        print self.canvas.mapSettings().extent().toString()
+        self.canvas.setDestinationCrs(QgsCoordinateReferenceSystem('EPSG:3857'))
+        self.canvas.setCrsTransformEnabled(True)
+        self.canvas.zoomToFullExtent()
+
+        print 'Extent: %s' % self.canvas.mapSettings().extent().toString()
+        # self._load_project()
         composition = self._load_template()
         # You must set the id in the template
         map_item = composition.getComposerItemById('map')
@@ -160,8 +172,7 @@ class PdfMaker(object):
         QgsProject.instance().clear()
 
 
-maker = PdfMaker(project_path='test.qgs', template_path='test.qpt')
-maker.make_pdf('/tmp/test.pdf')
-maker.make_pdf('/tmp/test.pdf')
+maker = PdfMaker(project_path='./projects/test.qgs', template_path='./templates/test.qpt')
+maker.make_pdf('test.pdf')
 
 
